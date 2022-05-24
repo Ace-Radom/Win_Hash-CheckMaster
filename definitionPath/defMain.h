3d4e73b5a11f4c508778bfc77582ede74d180d2c
Sichen Lyu <104905842+Ace-Radom@users.noreply.github.com>
2022-05-24 14:41:40 +0200

    Add files via upload

---
+++ definitionPath/defMain.h
@@ -0,0 +1,14 @@
+#ifndef _DEFMAIN_H_
+#define _DEFMAIN_H_
+
+#define _ERROR_ 0x00
+
+#define MD5 0x01
+#define SHA1 0x02
+#define SHA256 0x03
+#define SHA512 0x04
+
+#define Program_Start 0x05
+
+
+#endif
