bac6266311a989075796d6c25737f907a2a1a468
Sichen Lyu <104905842+Ace-Radom@users.noreply.github.com>
2022-05-30 16:29:36 +0200

    Update Format

--- src/OSRead.ps1
+++ src/OSRead.ps1
@@ -6,7 +6,7 @@ Write-Output "[OS]> OS Version: $( ( Get-Content $logPath -Tail 3 )[-3] )"
 Get-CimInstance -ClassName Win32_Processor | Select-Object -property Name | Out-file $logPath
 Write-Output "[OS]> CPU: $( ( Get-Content $logPath -Tail 3 )[-3] )"
 #get RAM
-$RAMinTotal = ( (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb )
+$RAMinTotal = ( ( Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum ).sum / 1gb )
 Write-Output "[OS]> Physic RAM: $( $RAMinTotal )"
 #delete log
 Remove-Item $logPath
