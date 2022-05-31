4558bae96afb85da462d35d1771e67f10c20c77c
Sichen Lyu <104905842+Ace-Radom@users.noreply.github.com>
2022-05-31 17:28:12 +0200

    Add files via upload

--- src/Command.cpp
+++ src/Command.cpp
@@ -1,13 +1,38 @@
 #include"Command.h"
 
 CommandData _CMDDATA[]{
-    { "-l"  , _l_ },
-    { "-d"  , _d_ },
-    { "-c"  , _c_ },
+    {},
+    { "-l " , _l_ },
+    { "-d " , _d_ },
+    { "-c " , _c_ },
     { "-ln" , _ln_ },
-    { "-e"  , _e_ }
+    { "-e " , _e_ }
 };
 
 void CommandCenter::read( std::string _Command ){
+    _cmd = _Command;
+    uint32_t _CMDBegin = _cmd.find( '-' );
+    setCommandNUM( _CMDBegin );
+    switch ( _CommandNUM ){
+        case _l_:
+            //_Checklist = _cmd.substr();
+            break;
+    
+    default:
+        break;
+    }
+}
 
+void CommandCenter::setCommandNUM( uint32_t _CMDBegin ){
+    for ( int i = 1 ; i <= DATALENGTH ; i++ )
+    {
+        if ( _cmd.substr( _CMDBegin , 2 ) == _CMDDATA[i]._Command )
+        {
+            _CommandNUM = _CMDDATA[i]._NUM;
+            std::cout << _CommandNUM << std::endl;
+            return;
+        }
+    }
+    _CommandNUM = _ERROR_;
+    return;
 }
--- src/Command.h
+++ src/Command.h
@@ -4,7 +4,9 @@
 #include"defMain.h"
 #include"typedefMain.h"
 #include<string>
-//#include<iostream>
+#include<iostream>
+
+#define DATALENGTH 5
 
 /* struct for saving commands */
 struct CommandData{
@@ -17,12 +19,16 @@ class CommandCenter{
         void read( std::string _Command );
         int getCommandNUM();
 
-    private:
-        int _CommandNUM;  /* the number of the command */
         std::string _Checklist;  /* Checklist path */
         std::string _WorkFolder;  /* Workfolder path */
         std::string _ChecklistName;  /* checklist name (-ln command) */
         std::string _Format;  /* given format (-e command) */
+
+    private:
+        int _CommandNUM;  /* the number of the command */
+        std::string _cmd;
+
+        void setCommandNUM( uint32_t _CMDBegin );
 };
 
 #endif
