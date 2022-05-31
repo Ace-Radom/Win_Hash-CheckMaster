f89d7987e878381245dd3369f4c47b703d44bbfa
Sichen Lyu <104905842+Ace-Radom@users.noreply.github.com>
2022-05-31 10:12:26 +0200

    Add main comment and powershell version get

--- src/OSRead.ps1
+++ src/OSRead.ps1
@@ -1,3 +1,5 @@
+#read OS details and powershell version
+
 $logPath = "$PSScriptRoot\certutil-precheckOutputLOG"
 #get OS version
 Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -property Version | Out-file $logPath
@@ -8,5 +10,8 @@ Write-Output "[OS]> CPU: $( ( Get-Content $logPath -Tail 3 )[-3] )"
 #get RAM
 $RAMinTotal = ( ( Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum ).sum / 1gb )
 Write-Output "[OS]> Physic RAM: $( $RAMinTotal )"
+#get powershell version
+$host | Select-Object -property Version | Out-file $logPath
+Write-Output "[OS]> Powershell Version: $( ( Get-Content $logPath -Tail 3 )[-3] )"
 #delete log
 Remove-Item $logPath
